57fc19c946d454a29b77b98c0df2a2d0c7d4c98c
Sushil Kumar <sushilkumar567895@gmail.com>
2024-02-26 11:24:18 +0530

    project upload

--- script.js
+++ script.js
@@ -1,20 +1,5 @@
-// ----------about----------
-let tablinks = document.getElementsByClassName("tab-links");
-let tabcontents = document.getElementsByClassName("tab-content");
-
-const opentab = (tabname) => {
-    for (tablink of tablinks) {
-        tablink.classList.remove("active-link");
-    }
-    for (tabcontent of tabcontents) {
-        tabcontent.classList.remove("active-tab");
-    }
-    event.currentTarget.classList.add("active-link");
-    document.getElementById(tabname).classList.add("active-tab");
-}
-
-// --------responsiveness-------
 var sidemenu = document.getElementById("sidemenu");
+
 const openmenu = () => {
     sidemenu.style.right = "0";
 }
